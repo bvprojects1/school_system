55a2a2ef4a29b29a8d28b457d49d2623d02b0a4a
fru.chifen <fru.chifen@bitsvalley.com>
2021-06-10 12:42:11 -0400

    part commit

---
+++ src/main/java/com/bitsvaley/micro/forms/AgentForm.java
@@ -0,0 +1,38 @@
+package com.bitsvaley.micro.forms;
+
+import com.bitsvaley.micro.domain.User;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+public class AgentForm extends User {
+
+    private int workingHrsPerWeek;
+    private LocalTime startTime;
+    private LocalTime closingTime;
+    private List<DayOfWeek> workingDays;
+    private LocalDate startDate = LocalDate.now();
+
+    public LocalDate getStartDate() { return startDate; }
+
+    public void setStartDate(LocalDate startDate) { this.startDate = startDate; }
+
+    public List<DayOfWeek> getWorkingDays() { return workingDays; }
+
+    public void setWorkingDays(List<DayOfWeek> workingDays) { this.workingDays = workingDays; }
+
+    public LocalTime getClosingTime() { return closingTime; }
+
+    public void setClosingTime(LocalTime closingTime) { this.closingTime = closingTime; }
+
+    public int getWorkingHrsPerWeek() { return workingHrsPerWeek; }
+
+    public void setWorkingHrsPerWeek(int workingHrsPerWeek) { this.workingHrsPerWeek = workingHrsPerWeek; }
+
+    public LocalTime getStartTime() { return startTime; }
+
+    public void setStartTime(LocalTime startTime) { this.startTime = startTime; }
+}
